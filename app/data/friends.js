19a413dc94fff32f16ddae9fad50e3d05dc4378e
Lynde Capogreco <lyndecapc@gmail.com>
2020-03-15 17:02:23 -0500

    friends array basic setup

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+//This file will hold the array of friends and will need to be exported.
+
+var friendsSurveyData = [
+    {
+        name: "Lynde",
+        imageUrl: ,
+        surveyScores: []
+    },
+    {
+        name: "Rudy",
+        imageUrl: ,
+        surveyScores: []
+    },
+    {
+        name: "Missie",
+        imageUrl: ,
+        surveyScores: [] 
+    },
+];
+
+//Code to export array so data is accessible to other files via require.
+
+module.exports = friendsSurveyData;
