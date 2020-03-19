a3656f3b59455afba9179e58a3e91ed665673c52
Lynde Capogreco <lyndecapc@gmail.com>
2020-03-19 14:12:42 -0500

    apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,43 @@
 var friendSurveyData = require("../data/friends.js");
 
+function apiRoutes(app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendSurveyData);
+    });
 
+    app.post("/api/friends", function (req, res) {
+        var newFriend = {
+            name: req.body.nme,
+            photo: req.body.photo,
+            scores: []
+        };
+        var scoresArray = [];
+        for (var i = 0; i < req.body.scores.length; I++) {
+            scoresArray.push(parseInt(req.body.scores[i]))
+        }
+        newFriend.scores = scoresArray;
 
+        var scoreComparisonArray = [];
+        for (var j = 0; j < newFriend.scores.length; j++) {
+            currentComparison += Math.abs(newFriend.scores[j] - friendsData[i].scores[j]);
+        }
+        scoreComparisonArray.push(currentComparison);
 
+        var bestMatchPosition = 0;
+        for (var i = 1; i < scoreComparisonArray.length; i++) {
+            if (scoreComparisonArray[i] <= scoreComparisonArray[bestMatchPosition]) {
+                bestMatchPosition = i;
+            }
+        }
 
+        var bestFriendMatch = friendsData[bestMatchPosition];
+
+        res.json(bestFriendMatch);
+
+        friendSurveyData.push(newFriend);
+
+    });
+}
 
 //export for use in server.js
 module.exports = apiRoutes;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,17 +4,13 @@ var path = require("path");
 //Two routes here GET and USE
 function htmlRoutes(app) {
 
-    app.get("/survey", function(req, res){
+    app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
-    app.use(function(req,res) {
+    app.use(function (req, res) {
         res.sendFile(path.join(__dirname + "/../public.home.html"));
     });
 }
-
-
-
-
 
 
 //export will be used in server.js file
