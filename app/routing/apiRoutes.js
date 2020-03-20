91333d864f3ae6d798dc3499fb3014e16cbc4975
Lynde Capogreco <lyndecapc@gmail.com>
2020-03-20 17:01:35 -0500

    can't get friendsData array toupdate

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,37 +6,59 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        var newFriend = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: []
+    var totalDifference = 0;
+    
+    
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
         };
-        var scoresArray = [];
-        for (var i = 0; i < req.body.scores.length; i++) {
-            scoresArray.push(parseInt(req.body.scores[i]))
+
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+
+        var b = userScores.map(function(item) {
+            return parseInt(item, 10);
+        });
+
+         userData = {
+             name: req.body.name,
+             photo: req.body.photo,
+             scores: b
+         };
+
+       console.log("Name: : " + userName);
+       console.log("Best match friend diff " + bestMatch.friendDifference);
+       console.log("==================================");
+
+       for (var i = 0; i < friendsData.length; i++) {
+           console.log(friendsData[i].name);
+           totalDifference = 0;
+           console.log("Total Diff " + totalDifference);
+           console.log("Best match friend diff " + bestMatch.friendDifference);
+
+        var bfriendScore = friendsData[i].scores.reduce((a,b) => a + b, 0);
+        console.log("Total friend score " + bfriendScore);
+        totalDifference += Math.abs(sum - bfriendScore);
+        console.log("==================> " + totalDifference);
+
+        if (totalDifference <= bestMatch.friendDifference) {
+            bestMatch.name = friendsData[i].name;
+            bestMatch.photo = friendsData[i].photo;
+            bestMatch.friendDifference = totalDifference;
         }
-        newFriend.scores = scoresArray;
+         console.log(totalDifference + " Total Difference");  
+       }
+        console.log(bestMatch);
 
-        var scoreComparisonArray = [];
-        for (var j = 0; j < newFriend.scores.length; j++) {
-            currentComparison += Math.abs(newFriend.scores[j] - friendsData[i].scores[j]);
-        }
-        scoreComparisonArray.push(currentComparison);
-
-        var bestMatchPosition = 0;
-        for (var i = 1; i < scoreComparisonArray.length; i++) {
-            if (scoreComparisonArray[i] <= scoreComparisonArray[bestMatchPosition]) {
-                bestMatchPosition = i;
-            }
-        }
-
-        var bestFriendMatch = friendsData[bestMatchPosition];
-
-        res.json(bestFriendMatch);
-
-        friendsData.push(newFriend);
+        friendsData.push(userData);
+        console.log("New user added");
+        console.log(userData);
+        res.json(bestMatch);
 
     });
-}
+};
 
-//export for use in server.js
+
